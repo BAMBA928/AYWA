fc81bc800d95d415af64a29c3b2fa5ac8c634d9f
BAMBA928 <bambapartou42@gmail.com>
2026-06-11 08:51:59 +0000

    js login1 modifier

--- script.js
+++ script.js
@@ -8,49 +8,68 @@ const inscrire = document.getElementById("btnInscription");
 const nom = document.getElementById("nom");
 const email = document.getElementById("email");
 const mdp = document.getElementById("password");
+const emailConnexion = document.getElementById("input1");
+const mdpConnexion = document.getElementById("input2");
+
 
 inscription.addEventListener(
     "click",
     () => {
-
-
-        
+        page1.style.display = "none";
+        page2.style.display = "flex";
     }
 );
 
 
-inscription.addEventListener(
+inscrire.addEventListener(
     "click",
     () => {
-    page1.style.display = "none";
-    page2.style.display = "flex";
+        const ustlisateur = {
+            nom: nom.value,
+            email: email.value,
+            mtdp: mdp.value
+        }
+
+        const ut = JSON.stringify(ustlisateur);
+        localStorage.setItem(
+            "bamba",
+            JSON.stringify(ustlisateur)
+        );
+
+        if (
+            nom.value === "" || email.value === "" || mdp.value === ""
+        ) {
+            alert("Veuillez remplir tous les champs");
+            return;
+        }
+
+        page2.style.display = "none";
+        page3.style.display = "flex";
     }
 );
 
 connecter.addEventListener(
     "click",
     () => {
-    page1.style.display = "none";
-    page3.style.display = "flex";
+        const bamba = JSON.parse(localStorage.getItem("bamba")
+        );
+        if (bamba === null) {
+            alert("Aucun compte enregistré");
+            return;
+        }
+        if (
+            emailConnexion.value === bamba.email &&
+            mdpConnexion.value === bamba.mtdp
+        ) {
+        page1.style.display = "none";
+        page3.style.display = "flex";
+        }
+        else {
+
+        }
+
     }
 );
-inscrire.addEventListener(
-    "click",
-    () => {
-            const ustlisateur ={
-        nom : nom.value,
-        email : email.value,
-        mtdp: mdp.value
-    }
-        const ut = JSON.stringify(ustlisateur);
-
-        
-    page2.style.display = "none";
-    page3.style.display = "flex";
-    }
-);
-
-
 
 
 // document.getElementById("insc").addEventListener("click", () => {
@@ -76,15 +95,15 @@ inscrire.addEventListener(
 //     motDePasse: "1234"
 // };
 // localStorage.setItem(
-//     "admin",
+//     "bamba",
 //     JSON.stringify(utilisateur)
 // );
 
 // const utilisateurJSON = JSON.stringify(utilisateur);
-// localStorage.setItem( "admin",utilisateurJSON );
-// const admin =
+// localStorage.setItem( "bamba",utilisateurJSON );
+// const bamba =
 //     JSON.parse(
-//         localStorage.getItem("admin")
+//         localStorage.getItem("bamba")
 //     );
 // const page1 = document.querySelector(".page1");
 // const page2 = document.querySelector(".page2");
